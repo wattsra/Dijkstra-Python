1a9e8eedeb0073be6f74879c76893d94cdb6c617
Robert Watts <poloking@gmail.com>
2020-05-19 15:57:29 +0100

    switched to using a matrix to hold graph

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
+#paths = {"S":"A","A":"S","S":"B","B":"S","S":"E","E":"S","A":"E","E":"A"}
+#paths_matrix = []
+#weighted_matrix =
 
 nodes = ["A","B","S","E"]
-paths = {"S":"A","A":"S","S":"B","B":"S","S":"E","E":"S","A":"E","E":"A"}
-lengths = {"SA":3,"AE":2,"SB":2,"SE":3}
-#tent = {}#each node gets a tentative value of infinity (using zero for ease)
-visitednodes = ["S"]
-currentnode = ["S"]
+
+paths = {"A":["S","E"],"B":["S"],"E":["A","S"],"S":["A","B","E"]}
+lengths = {"AS":3,"AE":2,"BS":2,"ES":3}
+
+
+paths_matrix = [[0,0,1,1],[0,0,1,0],[1,1,0,1],[1,0,1,0]]
+dist_matrix =  [[0,0,3,2],[0,0,2,0],[3,2,0,3],[2,0,3,0]]
+
+start = currentnode = "S"
+visitednodes = [start]
+end = "E"
+visit = ""
 
 def buildTent(nodes):
     tent = {}
@@ -13,8 +23,13 @@ def buildTent(nodes):
     return tent
 
 tent = buildTent(nodes)
+
+currentnodedistances = dist_matrix[nodes.index(currentnode)]
+print(currentnodedistances)
+print(tent)
+for i in range(len(currentnodedistances)):
+    if i != 0:
+        tent[nodes[i]] = currentnodedistances[i]
 print(tent)
 
-while currentnode =! "E":
-    for
 
