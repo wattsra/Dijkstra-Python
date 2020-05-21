88e566d493df9d903a2138368ee509692e2cd8b8
Robert Watts <poloking@gmail.com>
2020-05-21 14:13:19 +0100

    working algorithm completed

--- app.py
+++ app.py
@@ -1,100 +1,90 @@
 import operator
 from itertools import compress
 
-nodes = ["A","B","E","S"]
+# nodes = ["A","B","E","S"]
+#
+# paths_matrix = [[0,0,1,1],
+#                 [0,0,0,1],
+#                 [1,0,0,1],
+#                 [1,1,1,0]]
+#
+# dist_matrix =  [[0,0,2,3],
+#                 [0,0,0,2],
+#                 [2,0,0,6],
+#                 [3,2,6,0]]
 
-paths_matrix = [[0,0,1,1],[0,0,0,1],[1,0,0,1],[1,1,1,0]]
-dist_matrix =  [[0,0,2,3],[0,0,0,2],[2,0,0,3],[3,2,3,0]]
+
+nodes = ["A","B","C","D","E","S"]
+
+                #A,B,C,D,E,S
+paths_matrix = [[0,0,1,0,1,1],#A
+                [0,0,0,1,1,1],#B
+                [1,0,0,0,1,0],#C
+                [0,1,0,0,0,1],#D
+                [1,1,1,0,0,0],#E
+                [1,1,0,1,0,0]]#S
+
+                #A,B,C,D,E,S
+dist_matrix =  [[0,0,4,0,2,3],#A
+                [0,0,0,2,1,4],#B
+                [4,0,0,0,4,0],#C
+                [0,2,0,0,0,3],#D
+                [2,1,4,0,0,0],#E
+                [3,4,0,3,0,0]]#S
 
 start = curr = "S"
 end = "E"
 vi = []
-unvi = nodes
+unvi = nodes[:]
 
 def buildTent(nodes):
     tent = {}
     for node in nodes:
-        tent[node] = (1e400,[])
-    tent[start] = (0,[])
+        tent[node] = (1e400,"")
+    tent[start] = (0,"")
     return tent
 
 tent = buildTent(nodes)
-conn = list(compress(nodes,paths_matrix[nodes.index(curr)]))
-conndist = list(compress(dist_matrix[nodes.index(curr)],paths_matrix[nodes.index(curr)]))
 
 while "E" not in vi:
+    conn = nodes[:]
+    conn = list(compress(conn,paths_matrix[nodes.index(curr)]))
+    conndist = dist_matrix[:]
+    conndist = list(compress(dist_matrix[nodes.index(curr)],paths_matrix[nodes.index(curr)]))
     for c in conn:
         if(tent[c][0]) == 1e400:
-            tent[c] = (conndist[conn.index(c)], [curr])
-        else: ## need to update to add on value from the current node rather than just select the distance from the current node.
-            route = tent[c][1]
-            route.append(curr) ## need to only keep track of the last position.  final answer can work back through
-            #route.append(curr)
-            tent[c] = (conndist[conn.index(c)], route)
-
-    print("tent", tent)
-    vi.append(curr)
-    print(vi)
-    unvi.pop(unvi.index(curr))
-    print(unvi)
-    if end in vi:
-        print("shortest path found")
-        quit()
-    for u in unvi:
-        if tent[u][0]== 1e400:#check if the lowest unvisited distance is infinity then there is no path
-            print("no path to finish")
-            quit()
-    # choose new current item
-    min = 1e400
-    for t in tent:
-        if t in vi:
+            tent[c] = (conndist[conn.index(c)], curr)
+        elif c in vi:
             pass
-        elif tent[t][0] < min:
-            min = tent[t][0]
-            curr = t
-    print(curr)
+        elif tent[c][0] > conndist[conn.index(c)]: ## need to update to add on value from the current node rather than just select the distance from the current node.
+            #route = tent[c][1]
+            #route.append(curr) ## need to only keep track of the last position.  final answer can work back through
+            #route.append(curr)
+            tent[c] = (conndist[conn.index(c)], curr)
 
-
-"""
-
-####restart looping through items
-
-conn = list(compress(nodes,paths_matrix[nodes.index(curr)]))
-conndist = list(compress(dist_matrix[nodes.index(curr)],paths_matrix[nodes.index(curr)]))
-print("conn",conn,"conndist",conndist)
-for c in conn:
-    if(tent[c][0]) == 1e400:
-        tent[c] = (conndist[conn.index(c)], [curr])
-    else:
-        route = tent[c][1]
-        route.append(curr)
-        #route.append(curr)
-        tent[c] = (conndist[conn.index(c)], route)
-
-print("tent", tent)
-
-vi.append(curr)
-print(vi)
-unvi.pop(unvi.index(curr))
-print(unvi)
-if end in vi:
-    print("shortest path found")
-    quit()
-for u in unvi:
-    if tent[u][0]== 1e400:#check if the lowest unvisited distance is infinity then there is no path
-        print("no path to finish")
+    vi.append(curr)
+    unvi.pop(unvi.index(curr))
+    if end in vi:
+        p = end
+        path = []
+        pathdist = 0
+        while p != start:
+            path.append(p)
+            pathdist += tent[p][0]
+            p = tent[p][1]
+        path.append(start)
+        path.reverse()
+        print("shortest path is:", path, "with a distance of", pathdist) #output final shortest path
         quit()
 
-# choose new current item
-min = 1e400
-for t in tent:
-    if t in vi:
-        break
-    if tent[t][0] < min:
-        min = tent[t][0]
-        curr = t
-print(curr)
+    ## build list of the shortest paths
+    mini = []
+    for u in unvi:
+        mini.append(tent[u][0])
 
+    # check if the lowest unvisited distance is infinity then there is no path
+    if min(mini) == 1e400:
+         print("no path to finish")
+         quit()
 
-
-"""
+    curr = unvi[mini.index(min(mini))] # choose new current item based on shortest unvisited distance
